0d3c1e167ad78f8704095208fcd6cf65edee4f42
NofarZiv <nofarziv13@gmail.com>
2024-03-08 18:26:13 -0330

    added a get route to login.

--- express_server.js
+++ express_server.js
@@ -139,6 +139,11 @@ app.post("/register", (req, res) => {
   res.redirect(`/urls`);
 });
 
+app.get("/login", (req, res) => {
+  user_id = users[req.cookies["user_id"]];
+  res.render("login");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
