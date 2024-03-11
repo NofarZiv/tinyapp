c2419fb48767298767eff06f6c399c8066054df3
NofarZiv <nofarziv13@gmail.com>
2024-03-11 14:50:41 -0230

    changed the urlDatabase object to keep track which urls belongs to which user and updated the endpoint accessing the urls, updated endpoints to allow only users to access, delete or edit.

--- express_server.js
+++ express_server.js
@@ -21,9 +21,25 @@ function userLookup(email) {
   return false;
 };
 
+function urlsForUser(id) {
+  let userUrlsObj = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      userUrlsObj[key] = urlDatabase[key].longURL;
+    }
+  }
+  return userUrlsObj;
+};
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 
 const users = {
@@ -55,9 +71,13 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  if (!req.cookies["user_id"]) {
+    return res.status(403).send("<html><body>You need to be logged in or to register to see the URLs.</body></html>");
+  }
+  let userUrls = urlsForUser(req.cookies["user_id"]);
   const templateVars = { 
     user_id: users[req.cookies["user_id"]],
-    urls: urlDatabase 
+    urls: userUrls
   };
   res.render("urls_index", templateVars);
 });
@@ -72,25 +92,33 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   if (!req.cookies["user_id"]) {
-    console.log(error);
     return res.status(403).send("<html><body>You need to be logged in to create a new URL.</body></html>");
   }
   let id = generateRandomString();
-  urlDatabase[id] = req.body.longURL; 
+  urlDatabase[id] = {
+    longURL: req.body.longURL,
+    userID: req.cookies["user_id"]
+  }; 
   res.redirect(`/urls/${id}`) 
 });
 
 app.get("/urls/:id", (req, res) => {
+  if (!req.cookies["user_id"]) {
+    return res.status(403).send("<html><body>Please log in to see the short url.</body></html>");
+  }
+  if (!urlDatabase[req.params.id] || urlDatabase[req.params.id].userID !== req.cookies["user_id"]) {
+    return res.status(404).send("<html><body>URL not found or you don't have permission to access this page.</body></html>");
+  }
   const templateVars = {
     user_id: users[req.cookies["user_id"]], 
     id: req.params.id, 
-    longURL: urlDatabase[req.params.id] 
+    longURL: urlDatabase[req.params.id].longURL
   };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
+  const longURL = urlDatabase[req.params.id].longURL;
   if (!longURL) {
     return res.status(404).send("<html><body>Shortened URL not found.</body></html>");
   }
@@ -98,20 +126,47 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.post("/urls/:id/delete", (req, res) => {
+  if (!req.cookies["user_id"]) {
+    return res.status(403).send("<html><body>Please log in to delete the URL.</body></html>");
+  }
+  if (!urlDatabase[req.params.id]) {
+    return res.status(404).send("<html><body>URL not found.</body></html>");
+  }
+  if (urlDatabase[req.params.id].userID !== req.cookies["user_id"]) {
+    return res.status(404).send("<html><body>You do not have premission to delete this URL.</body></html>");
+  }
   delete urlDatabase[req.params.id]; 
   res.redirect(`/urls`); 
 });
 
 app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
+  if (!req.cookies["user_id"]) {
+    return res.status(403).send("<html><body>Please log in to view the URLs.</body></html>");
+  }
+  if (!urlDatabase[req.params.id]) {
+    return res.status(404).send("<html><body>URL not found.</body></html>");
+  }
+  if (urlDatabase[req.params.id].userID !== req.cookies["user_id"]) {
+    return res.status(404).send("<html><body>You do not have premission to access this URL.</body></html>");
+  }
+  urlDatabase[req.params.id].longURL = req.body.longURL;
   res.redirect(`/urls`); 
 });
 
 app.post("/urls/:id/edit", (req, res) => {
+  if (!req.cookies["user_id"]) {
+    return res.status(403).send("<html><body>Please log in to edit this URL.</body></html>");
+  }
+  if (!urlDatabase[req.params.id]) {
+    return res.status(404).send("<html><body>URL not found.</body></html>");
+  }
+  if (urlDatabase[req.params.id].userID !== req.cookies["user_id"]) {
+    return res.status(404).send("<html><body>You do not have premission to edit this URL.</body></html>");
+  }
   const vars = { 
     user_id: users[req.cookies["user_id"]],
     id: req.params.id, 
-    longURL: urlDatabase[req.params.id] 
+    longURL: urlDatabase[req.params.id].longURL 
   };
   res.render("urls_show", vars); 
 });
