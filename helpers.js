5e4ba1eae60648df9dd69f63f5daa00b7521b455
NofarZiv <nofarziv13@gmail.com>
2024-03-12 09:47:09 -0230

    added a couple more functions to the file.

--- helpers.js
+++ helpers.js
@@ -8,6 +8,19 @@ const getUserByEmail = function(email, database) {
   return undefined;
 };
 
+const urlsForUser = function(id, database) {
+  let userUrlsObj = {};
+  for (let key in database) {
+    if (database[key].userID === id) {
+      userUrlsObj[key] = database[key].longURL;
+    }
+  }
+  return userUrlsObj;
+};
+
+const generateRandomString = function() {
+  return Math.random().toString(36).slice(2).substring(0, 6);
+};
 
 
-module.exports = getUserByEmail;
+module.exports = { getUserByEmail, urlsForUser, generateRandomString };
